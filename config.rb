d6f71b9830a74be1ae181812587123f42efa9363
Mark Koellmann <mark@koellmann.eu>
2016-11-10 19:19:11 -0500

    Updated menu for index

--- config.rb
+++
@@ -1,9 +0,0 @@
-preferred_syntax = :scss
-http_path = '/'
-css_dir = 'assets/css'
-sass_dir = 'assets/scss'
-images_dir = 'assets/img'
-javascripts_dir = 'assets/js'
-relative_assets = true
-line_comments = false
-# output_style = :compressed
