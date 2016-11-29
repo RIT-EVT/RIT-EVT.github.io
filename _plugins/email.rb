9598c7141a266b807b49ef7be74441f259d43d67
Wheeler Law <whelderwheels613@gmail.com>
2016-11-29 02:32:43 -0500

    Email obfuscation completely works

--- _plugins/email.rb
+++ _plugins/email.rb
@@ -28,58 +28,78 @@ module Jekyll
       if @email_var
         @email = resolve(context, @email_var)
       end
-      reversed = encode(@email.each_char.to_a.reverse.join)
+      reversed = @email.each_char.to_a.reverse.join
+      reversed_encoded = encode(reversed)
       obfuscated = encode(@email)
 
       copyFunction = %(
-        document.copyEmail = function(event, text){
-          var regexSymbolWithCombiningMarks = /([\0-\u02FF\u0370-\u1AAF\u1B00-\u1DBF\u1E00-\u20CF\u2100-\uD7FF\uE000-\uFE1F\uFE30-\uFFFF]|[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF])([\u0300-\u036F\u1AB0-\u1AFF\u1DC0-\u1DFF\u20D0-\u20FF\uFE20-\uFE2F]+)/g;
-          var regexSurrogatePair = /([\uD800-\uDBFF])([\uDC00-\uDFFF])/g;
-          var reverse = function(string) {
-            string = string
-              .replace(regexSymbolWithCombiningMarks, function($0, $1, $2) {return reverse($2) + $1;})
-              .replace(regexSurrogatePair, '$2$1');
-            var result = '';
-            var index = string.length;
-            while (index--) {
-              result += string.charAt(index);
-            }
-            return result;
-          };
-          text = reverse(text);
-          if (window.clipboardData && window.clipboardData.setData) {
-            // IE specific code path to prevent textarea being shown while dialog is visible.
-            return clipboardData.setData("Text", text);
+        document.copyEmail = function (event, text) {
+            var regexSymbolWithCombiningMarks = /([\\0-\\u02FF\\u0370-\\u1AAF\\u1B00-\\u1DBF\\u1E00-\\u20CF\\u2100-\\uD7FF\\uE000-\\uFE1F\\uFE30-\\uFFFF]|[\\uD800-\\uDBFF][\\uDC00-\\uDFFF]|[\\uD800-\\uDBFF](?![\\uDC00-\\uDFFF])|(?:[^\\uD800-\\uDBFF]|^)[\\uDC00-\\uDFFF])([\\u0300-\\u036F\\u1AB0-\\u1AFF\\u1DC0-\\u1DFF\\u20D0-\\u20FF\\uFE20-\\uFE2F]+)/g;
+            var regexSurrogatePair = /([\\uD800-\\uDBFF])([\\uDC00-\\uDFFF])/g;
+            var reverse = function (string) {
+                string = string
+                    .replace(regexSymbolWithCombiningMarks, function ($0, $1, $2) {
+                        return reverse($2) + $1;
+                    })
+                    .replace(regexSurrogatePair, '$2$1');
+                var result = '';
+                var index = string.length;
+                while (index--) {
+                    result += string.charAt(index);
+                }
+                return result;
+            };
+            text = reverse(text);
+            if (window.clipboardData && window.clipboardData.setData) {
+                // IE specific code path to prevent textarea being shown while dialog is visible.
+                return clipboardData.setData("Text", text);
 
-          } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
-            var textarea = document.createElement("textarea");
-            textarea.textContent = text;
-            textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
-            document.body.appendChild(textarea);
-            textarea.select();
-            try {
-                return document.execCommand("copy");  // Security exception may be thrown by some browsers.
-            } catch (ex) {
-                console.warn("Copy to clipboard failed.", ex);
-                return false;
-            } finally {
-                document.body.removeChild(textarea);
+            } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+                var textarea = document.createElement("textarea");
+                textarea.textContent = text;
+                textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
+                document.body.appendChild(textarea);
+                textarea.select();
+                try {
+                    return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+                } catch (ex) {
+                    console.warn("Copy to clipboard failed.", ex);
+                    return false;
+                } finally {
+                    document.body.removeChild(textarea);
+                    setTimeout(function(){
+                        var doc = document, text = event.target, range, selection;
+                        if (doc.body.createTextRange) {
+                            range = document.body.createTextRange();
+                            range.moveToElementText(text);
+                            range.select();
+                        } else if (window.getSelection) {
+                            selection = window.getSelection();
+                            range = document.createRange();
+                            range.selectNodeContents(text);
+                            selection.removeAllRanges();
+                            selection.addRange(range);
+                        }
+                    }, 1);
+
+                }
             }
-          }
-        }
+        };
       )
       if @style == "link"
         %(
-          <script type=\"text/javascript\">
+          <script type="text/javascript">
           #{copyFunction}
-          document.write('<a style=\"#{STYLE}\" href=\"#{MAIL_TO}#{obfuscated}\">#{reversed}</a>');
+          var temp = "#{reversed}";
+          document.write('<a oncopy="copyEmail(event, temp)" style="#{STYLE}" href="#{MAIL_TO}#{obfuscated}">#{reversed_encoded}</a>');
           </script>
         )
       elsif @style == "text"
         %(
-          <script type=\"text/javascript\">
+          <script type="text/javascript">
           #{copyFunction}
-          document.write('<span oncopy=\"copyEmail(event, #{reversed})\" style=\"#{STYLE}\">#{reversed}</span>');
+          var temp = "#{reversed}";
+          document.write('<span oncopy="copyEmail(event, temp)" style="#{STYLE}">#{reversed_encoded}</span>');
           </script>
         )
       else
