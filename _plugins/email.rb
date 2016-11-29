8cb6a493d47f52cb3d0ed12dbabee09212cb1de0
Wheeler Law <whelderwheels613@gmail.com>
2016-11-29 02:34:18 -0500

    Style standards

--- _plugins/email.rb
+++ _plugins/email.rb
@@ -32,7 +32,7 @@ module Jekyll
       reversed_encoded = encode(reversed)
       obfuscated = encode(@email)
 
-      copyFunction = %(
+      copy_function = %(
         document.copyEmail = function (event, text) {
             var regexSymbolWithCombiningMarks = /([\\0-\\u02FF\\u0370-\\u1AAF\\u1B00-\\u1DBF\\u1E00-\\u20CF\\u2100-\\uD7FF\\uE000-\\uFE1F\\uFE30-\\uFFFF]|[\\uD800-\\uDBFF][\\uDC00-\\uDFFF]|[\\uD800-\\uDBFF](?![\\uDC00-\\uDFFF])|(?:[^\\uD800-\\uDBFF]|^)[\\uDC00-\\uDFFF])([\\u0300-\\u036F\\u1AB0-\\u1AFF\\u1DC0-\\u1DFF\\u20D0-\\u20FF\\uFE20-\\uFE2F]+)/g;
             var regexSurrogatePair = /([\\uD800-\\uDBFF])([\\uDC00-\\uDFFF])/g;
@@ -89,7 +89,7 @@ module Jekyll
       if @style == "link"
         %(
           <script type="text/javascript">
-          #{copyFunction}
+          #{copy_function}
           var temp = "#{reversed}";
           document.write('<a oncopy="copyEmail(event, temp)" style="#{STYLE}" href="#{MAIL_TO}#{obfuscated}">#{reversed_encoded}</a>');
           </script>
@@ -97,7 +97,7 @@ module Jekyll
       elsif @style == "text"
         %(
           <script type="text/javascript">
-          #{copyFunction}
+          #{copy_function}
           var temp = "#{reversed}";
           document.write('<span oncopy="copyEmail(event, temp)" style="#{STYLE}">#{reversed_encoded}</span>');
           </script>
